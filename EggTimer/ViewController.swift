f18627333d6aa0aed5e20f5ddd10905411d5cba9
EKTEST <e_zhitnikov@bk.ru>
2023-03-30 18:22:49 +0400

    added alarm sound

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -6,18 +6,19 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var titleLable: UILabel!
     
-    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 5]
     var timer = Timer()
     var totalTime = 0
     var secondPassed = 0
+    var player: AVAudioPlayer!
 
-    
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
         timer.invalidate()
@@ -27,20 +28,21 @@ class ViewController: UIViewController {
         secondPassed = 0
         titleLable.text = hardness
         
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target:self, selector: #selector(updateTimer), userInfo:nil, repeats: true)
     }
     
     @objc func updateTimer() {
         if secondPassed < totalTime {
             secondPassed += 1
-            
-            progressBar.progress = Float(secondPassed)/Float(totalTime)
-            
-
+            progressBar.progress = Float(secondPassed) / Float(totalTime)
+        
         } else {
             timer.invalidate()
-            progressBar.progress = 1
             titleLable.text = "DONE!"
+            
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
         }
     }
 
