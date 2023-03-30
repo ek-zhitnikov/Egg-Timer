1fb445f10c55e544790cfd5a2b569e0b8ade0c31
EKTEST <e_zhitnikov@bk.ru>
2023-03-30 16:13:35 +0400

    added connections

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,9 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBOutlet weak var titleLable: UILabel!
+    
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    
+    var secondsRemaning = 60
+    
+    var timer = Timer()
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        
+        progressBar.progress = 1
+        
+        let hardness = sender.currentTitle!
+
+        secondsRemaning = eggTimes[hardness]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if secondsRemaning > 0{
+            print("\(secondsRemaning) seconds.")
+            secondsRemaning -= 1
+        } else {
+            timer.invalidate()
+            titleLable.text = "DONE!"
+        }
     }
 
 
